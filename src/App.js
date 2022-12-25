8da941b43f39fded17387dccade673e6c65644d5
sookkiong <solnaamu98@gmail.com>
2022-12-26 00:00:45 +0900

    fix: footer를 layout으로 만들어서 공통 레이아웃으로 빼기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import PageHeader from "./layout/PageHeader";
 import GlobalStyle from "./GlobalStyle";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route index element={<About />} />
           </Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
