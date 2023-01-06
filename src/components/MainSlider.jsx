2962a680ffa4211c449fc5c47a13015aa539166f
sookkiong <solnaamu98@gmail.com>
2023-01-06 17:01:13 +0900

    feat: slick 슬라이더 스타일 적용 문제

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -1,11 +1,11 @@
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { MainSliderItem } from "./Main";
+import "../slick/slick.css";
+import "../slick/slick-theme.css";
 
 const MainSlider = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -16,12 +16,20 @@ const MainSlider = () => {
     pauseOnHover: false,
   };
   return (
-    <div>
+    <div
+      style={{
+        overflow: "hidden",
+        width: "60%",
+        height: "65%",
+        zIndex: "50",
+        marginRight: "15%",
+      }}
+    >
       <Slider {...settings}>
         {MainSliderItem.map((slide) => {
           return (
             <div>
-              <img src={slide.img} alt="슬라이드 이미지" />
+              <img src={slide.img} alt="슬라이드 이미지" width="100%" />
             </div>
           );
         })}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,52 @@
 import { SectionsContainer, Section } from "react-fullpage";
 import MainSlider from "../components/MainSlider";
+import styled from "styled-components";
 
 const MainPage = () => {
   let options = {
-    anchors: ["sectionOne", "sectionTwo", "sectionThree"],
+    anchors: ["home", "onAir", "works", "issue", "news"],
+    delay: 500,
+    navigation: true,
   };
 
   return (
     <SectionsContainer {...options}>
       <Section>
-        <div>
+        <div
+          style={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            background: 'url("/img/bg.png") no-repeat center center',
+            backgroundSize: "cover",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              zIndex: "100",
+              color: "#fff",
+              position: "absolute",
+              left: "13%",
+              fontSize: "30px",
+            }}
+          >
+            <span
+              style={{
+                fontWeight: "600",
+                display: "block",
+                fontSize: "68px",
+                fontFamily: "YDestreet",
+              }}
+            >
+              ASIA NO.1 STUDIO
+            </span>
+            아시아의 콘텐츠와 라이프스타일을
+            <br />
+            선도합니다.
+          </div>
           <MainSlider />
         </div>
       </Section>
@@ -18,6 +55,12 @@ const MainPage = () => {
       </Section>
       <Section>
         <div>섹션3</div>
+      </Section>
+      <Section>
+        <div>섹션4</div>
+      </Section>
+      <Section>
+        <div>섹션5</div>
       </Section>
     </SectionsContainer>
   );
